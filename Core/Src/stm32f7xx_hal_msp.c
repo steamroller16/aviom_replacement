96d0135335e84fe3c1d32b56041fec01a516e4ae
SSoundar <SSoundar@slb.com>
2020-12-21 16:09:15 -0600

    Reduced ethernet interrupt priority.

--- Core/Src/stm32f7xx_hal_msp.c
+++ Core/Src/stm32f7xx_hal_msp.c
@@ -138,7 +138,7 @@ void HAL_ETH_MspInit(ETH_HandleTypeDef* heth)
     HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
 
     /* ETH interrupt Init */
-    HAL_NVIC_SetPriority(ETH_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(ETH_IRQn, 1, 0);
     HAL_NVIC_EnableIRQ(ETH_IRQn);
   /* USER CODE BEGIN ETH_MspInit 1 */
 
