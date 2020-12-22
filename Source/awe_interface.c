9eea7f89bdfedef9778884621661a3ffddaa763b
SSoundar <SSoundar@slb.com>
2020-12-22 15:32:10 -0600

    Lots of minor cleanup in awe_interface.c.

--- Source/awe_interface.c
+++ Source/awe_interface.c
@@ -19,58 +19,48 @@ Description :
 
 /* Local Macros/Constants/Structures -----------------------------------------*/
 // Version Information
-#define USER_VER 20190801
+#define USER_VERSION            (20201222) // 2020/12/22
 
 // Various parameters
-#define NUM_CORES 1
-#define CORE_ID 0
-#define CORE_SPEED 216e6f
-#define SAMPLE_SPEED 216e6f
-#define HAS_FLOAT_SUPPORT 1
-#define HAS_FLASH_FILESYSTEM 1
-#define HAS_GPIO 1
-#define NO_INPUT_PINS 1
-#define NO_OUTPUT_PINS 1
-#define IS_SMP 0
-#define NUM_AUDIO_THREADS 2
-#define AUDIO_SAMPLE_RATE 48000.0f
-#define AUDIO_BLOCK_SIZE 32
-#define IS_COMPLEX 0
-#define SAMPLE_SIZE_IN_BYTES 4
+#define INSTANCE_ID             (0)
+#define CORE_SPEED              (192e6f)
+#define PROFILE_SPEED           (192e6f)
+#define NUM_AUDIO_THREADS       (1)
+#define AUDIO_SAMPLE_RATE       (48000.0f)
+#define AUDIO_BLOCK_SIZE        (32)
 
 // Specify the size of each of the heaps on this target
-#define MASTER_HEAP_SIZE		(1024*5)
-#define FASTB_HEAP_SIZE			(1024*5)
-#define SLOW_HEAP_SIZE			(1024*5)
+#define MASTER_HEAP_SIZE        (1024*5)
+#define FASTB_HEAP_SIZE         (1024*5)
+#define SLOW_HEAP_SIZE          (1024*5)
 
 /* Define number of input and output channels */
-#define NUM_INPUT_CHANNELS      2
-#define NUM_OUTPUT_CHANNELS     2
+#define NUM_INPUT_CHANNELS      (2)
+#define NUM_OUTPUT_CHANNELS     (2)
 
 
 /* Public Global Variables ---------------------------------------------------*/
+
+
+/* Private Global Variables --------------------------------------------------*/
 // AWE instance
-AWEInstance g_AWEInstance;
+static AWEInstance AweInstance;
 
 // Heap
-UINT32 g_MasterHeap[MASTER_HEAP_SIZE];
-UINT32 g_FastbHeap[FASTB_HEAP_SIZE];
-UINT32 g_SlowHeap[SLOW_HEAP_SIZE];
+static UINT32 MasterHeap[MASTER_HEAP_SIZE];
+static UINT32 FastbHeap[FASTB_HEAP_SIZE];
+static UINT32 SlowHeap[SLOW_HEAP_SIZE];
 
 // Module table
-const void * g_module_descriptor_table[] =
+static const void * ModuleDescriptorTable[] =
 {
     // List of modules from ModuleList.h
     (void *)LISTOFCLASSOBJECTS
 };
-UINT32 g_module_descriptor_table_size = sizeof(g_module_descriptor_table) / sizeof(g_module_descriptor_table[0]);
+static const UINT32 ModuleDescriptorTableSize = sizeof(ModuleDescriptorTable) / sizeof(ModuleDescriptorTable[0]);
 
-
-/* Private Global Variables --------------------------------------------------*/
-// The input pin
-static IOPinDescriptor s_InputPin = { 0 };
-
-// The output pin.
+// AWE IO pins (not physical GPIO pins)
+static IOPinDescriptor s_InputPin =  { 0 };
 static IOPinDescriptor s_OutputPin = { 0 };
 
 
@@ -78,57 +68,53 @@ static IOPinDescriptor s_OutputPin = { 0 };
 
 
 /* Function Implementations --------------------------------------------------*/
-//-----------------------------------------------------------------------------
-// METHOD:  AWEInstanceInit
-// PURPOSE: Initialize AWE Instance with target details
-//-----------------------------------------------------------------------------
 void AweInterface_Init(void)
 {
+    // Setup execution cycle counter (ARM stuff)
     CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
     DWT->LAR = 0xC5ACCE55;
     DWT->CYCCNT = 0;
     DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
 
-    memset(&g_AWEInstance, 0, sizeof(AWEInstance) );
+    // Zero out AWE instance struct
+    memset(&AweInstance, 0, sizeof(AWEInstance) );
 
-    g_AWEInstance.pInputPin = &s_InputPin;
-    g_AWEInstance.pOutputPin = &s_OutputPin;
-
+    // Setup AWE IO pins
+    AweInstance.pInputPin = &s_InputPin;
+    AweInstance.pOutputPin = &s_OutputPin;
     awe_initPin(&s_InputPin, NUM_INPUT_CHANNELS, NULL);
     awe_initPin(&s_OutputPin, NUM_OUTPUT_CHANNELS, NULL);
 
-    // User version word
-    g_AWEInstance.userVersion = USER_VER;
+    // Fill in various parameters
+    AweInstance.userVersion = USER_VERSION;
+    AweInstance.instanceId = INSTANCE_ID;
+    AweInstance.coreSpeed = CORE_SPEED;
+    AweInstance.profileSpeed = PROFILE_SPEED;
+    AweInstance.pName = "ST32F767";
+    AweInstance.numThreads = NUM_AUDIO_THREADS;
+    AweInstance.pModuleDescriptorTable = ModuleDescriptorTable;
+    AweInstance.numModules = ModuleDescriptorTableSize;
+    AweInstance.sampleRate = AUDIO_SAMPLE_RATE;
+    AweInstance.fundamentalBlockSize = AUDIO_BLOCK_SIZE;
 
-    g_AWEInstance.instanceId = CORE_ID;
-    g_AWEInstance.coreSpeed = CORE_SPEED;
-    g_AWEInstance.profileSpeed = CORE_SPEED;
-    g_AWEInstance.pName = "ST32F767";
-    g_AWEInstance.numThreads = NUM_AUDIO_THREADS;
-    g_AWEInstance.pModuleDescriptorTable = g_module_descriptor_table;
-    g_AWEInstance.numModules = g_module_descriptor_table_size;
-    g_AWEInstance.sampleRate = AUDIO_SAMPLE_RATE;
-    g_AWEInstance.fundamentalBlockSize = AUDIO_BLOCK_SIZE;
+    // Define the heap sizes and ram
+    AweInstance.fastHeapASize = MASTER_HEAP_SIZE;
+    AweInstance.fastHeapBSize = FASTB_HEAP_SIZE;
+    AweInstance.slowHeapSize  = SLOW_HEAP_SIZE;
+    AweInstance.pFastHeapA = MasterHeap;
+    AweInstance.pFastHeapB = FastbHeap;
+    AweInstance.pSlowHeap  = SlowHeap;
 
-    // Define the heap sizes
-    g_AWEInstance.fastHeapASize = MASTER_HEAP_SIZE;
-    g_AWEInstance.fastHeapBSize = FASTB_HEAP_SIZE;
-    g_AWEInstance.slowHeapSize  = SLOW_HEAP_SIZE;
+    // Tuning interface buffers
+    AweInstance.pPacketBuffer = AweTuningHwInterface_GetPacketBufferPointer();
+    AweInstance.pReplyBuffer = AweTuningHwInterface_GetReplyBufferPointer();
+    AweInstance.packetBufferSize = AweTuningHwInterface_GetMaxPacketBufferSizeWords();
 
-    // Point to the heaps on this target
-    g_AWEInstance.pFastHeapA = g_MasterHeap;
-    g_AWEInstance.pFastHeapB = g_FastbHeap;
-    g_AWEInstance.pSlowHeap  = g_SlowHeap;
-
-    g_AWEInstance.pPacketBuffer = AweTuningHwInterface_GetPacketBufferPointer();
-    g_AWEInstance.pReplyBuffer = AweTuningHwInterface_GetReplyBufferPointer();
-    g_AWEInstance.packetBufferSize = AweTuningHwInterface_GetMaxPacketBufferSizeWords();
-
-    // Initialize AWE signal processing instance
-    awe_init(&g_AWEInstance);
+    // Init AWE signal processing instance
+    awe_init(&AweInstance);
 
     // Init tuning hardware interface
-    AweTuningHwInterface_Init(&g_AWEInstance);
+    AweTuningHwInterface_Init(&AweInstance);
 }
 
 #if 0
@@ -147,8 +133,8 @@ void BSP_AUDIO_IN_DMA_Handler(void)
     // Collected USB playback samples from an ASRC component
     pUSBSamples = USB_Playback_ASRCSamples;
 
-    bAudioIsStarted = awe_audioIsStarted(&g_AWEInstance);
-    bLayoutValid = awe_layoutIsValid(&g_AWEInstance);
+    bAudioIsStarted = awe_audioIsStarted(&AweInstance);
+    bLayoutValid = awe_layoutIsValid(&AweInstance);
 
     // If no audio processing running
     if (!bAudioIsStarted)
@@ -167,20 +153,20 @@ void BSP_AUDIO_IN_DMA_Handler(void)
         else
         {
             // Insert the received USB samples into the AudioWeaver buffer
-            awe_audioImportSamples(&g_AWEInstance, pUSBSamples, STRIDE2, CHANNEL1, Sample16bit);
-            awe_audioImportSamples(&g_AWEInstance, &pUSBSamples[1], STRIDE2, CHANNEL2, Sample16bit);
+            awe_audioImportSamples(&AweInstance, pUSBSamples, STRIDE2, CHANNEL1, Sample16bit);
+            awe_audioImportSamples(&AweInstance, &pUSBSamples[1], STRIDE2, CHANNEL2, Sample16bit);
 
             // Insert the received CODEC samples into the AudioWeaver buffer
-            awe_audioImportSamples(&g_AWEInstance, &AudioBufferIn[nInReadNdx], STRIDE2, CHANNEL3, Sample16bit);
-            awe_audioImportSamples(&g_AWEInstance, &AudioBufferIn[nInReadNdx + 1], STRIDE2, CHANNEL4, Sample16bit);
+            awe_audioImportSamples(&AweInstance, &AudioBufferIn[nInReadNdx], STRIDE2, CHANNEL3, Sample16bit);
+            awe_audioImportSamples(&AweInstance, &AudioBufferIn[nInReadNdx + 1], STRIDE2, CHANNEL4, Sample16bit);
 
             // Insert the received Mic samples into the AudioWeaver buffer
-            awe_audioImportSamples(&g_AWEInstance, &MicBufferIn[nMicReadBufferNdx], STRIDE1, CHANNEL5, Sample16bit);
-            awe_audioImportSamples(&g_AWEInstance, &MicBufferIn[nMicReadBufferNdx + AUDIO_BLOCK_SIZE], STRIDE1, CHANNEL6, Sample16bit);
-            awe_audioImportSamples(&g_AWEInstance, &MicBufferIn[nMicReadBufferNdx + (AUDIO_BLOCK_SIZE * 2)], STRIDE1, CHANNEL7, Sample16bit);
-            awe_audioImportSamples(&g_AWEInstance, &MicBufferIn[nMicReadBufferNdx + (AUDIO_BLOCK_SIZE * 3)], STRIDE1, CHANNEL8, Sample16bit);
+            awe_audioImportSamples(&AweInstance, &MicBufferIn[nMicReadBufferNdx], STRIDE1, CHANNEL5, Sample16bit);
+            awe_audioImportSamples(&AweInstance, &MicBufferIn[nMicReadBufferNdx + AUDIO_BLOCK_SIZE], STRIDE1, CHANNEL6, Sample16bit);
+            awe_audioImportSamples(&AweInstance, &MicBufferIn[nMicReadBufferNdx + (AUDIO_BLOCK_SIZE * 2)], STRIDE1, CHANNEL7, Sample16bit);
+            awe_audioImportSamples(&AweInstance, &MicBufferIn[nMicReadBufferNdx + (AUDIO_BLOCK_SIZE * 3)], STRIDE1, CHANNEL8, Sample16bit);
 
-            layoutMask = awe_audioGetPumpMask(&g_AWEInstance);
+            layoutMask = awe_audioGetPumpMask(&AweInstance);
 
             if (layoutMask > 0)
             {
@@ -204,12 +190,12 @@ void BSP_AUDIO_IN_DMA_Handler(void)
             }
 
             // Insert the processed Audio Weaver samples into the CODEC output buffer
-            awe_audioExportSamples(&g_AWEInstance,  &AudioBufferOut[nOutWriteNdx], STRIDE2, CHANNEL1, Sample16bit);
-            awe_audioExportSamples(&g_AWEInstance,  &AudioBufferOut[nOutWriteNdx + 1], STRIDE2, CHANNEL2, Sample16bit);
+            awe_audioExportSamples(&AweInstance,  &AudioBufferOut[nOutWriteNdx], STRIDE2, CHANNEL1, Sample16bit);
+            awe_audioExportSamples(&AweInstance,  &AudioBufferOut[nOutWriteNdx + 1], STRIDE2, CHANNEL2, Sample16bit);
 
             // Insert the processed Audio Weaver samples into the USB output buffer
-            awe_audioExportSamples(&g_AWEInstance,  USB_Record_ASRCSamples, STRIDE2, CHANNEL3, Sample16bit);
-            awe_audioExportSamples(&g_AWEInstance,  &USB_Record_ASRCSamples[1], STRIDE2, CHANNEL4, Sample16bit);
+            awe_audioExportSamples(&AweInstance,  USB_Record_ASRCSamples, STRIDE2, CHANNEL3, Sample16bit);
+            awe_audioExportSamples(&AweInstance,  &USB_Record_ASRCSamples[1], STRIDE2, CHANNEL4, Sample16bit);
         }
     }
 
@@ -233,7 +219,7 @@ void AudioWeaverPump_IRQHandler1(void)
 
     NVIC_ClearPendingIRQ(AudioWeaverPump_IRQ1);
 
-    g_bDeferredProcessingRequired |= awe_audioPump(&g_AWEInstance, 0);
+    g_bDeferredProcessingRequired |= awe_audioPump(&AweInstance, 0);
 
     g_bAudioPump1Active = FALSE;
 
@@ -250,7 +236,7 @@ void AudioWeaverPump_IRQHandler2(void)
 
     NVIC_ClearPendingIRQ(AudioWeaverPump_IRQ2);
 
-    g_bDeferredProcessingRequired |= awe_audioPump(&g_AWEInstance, 1);
+    g_bDeferredProcessingRequired |= awe_audioPump(&AweInstance, 1);
 
     g_bAudioPump2Active = FALSE;
 
@@ -273,14 +259,14 @@ void AWEIdleLoop(void)
             g_bPacketReceived = FALSE;
 
             // Process the received packet
-            awe_packetProcess(&g_AWEInstance);
+            awe_packetProcess(&AweInstance);
 
             // Send the reply over USB HID
-            USBSendReply(&g_AWEInstance);
+            USBSendReply(&AweInstance);
         }
 
        // Process any local controls
-        if (awe_audioIsStarted(&g_AWEInstance) )
+        if (awe_audioIsStarted(&AweInstance) )
         {
             UINT32 classID;
             INT32 nValue;
@@ -289,29 +275,29 @@ void AWEIdleLoop(void)
             if (g_bDeferredProcessingRequired || bMoreProcessingRequired)
             {
                 g_bDeferredProcessingRequired = FALSE;
-                bMoreProcessingRequired = awe_deferredSetCall(&g_AWEInstance);
+                bMoreProcessingRequired = awe_deferredSetCall(&AweInstance);
             }
 
             // Does the current AWE model have a SinkInt module with this control object ID?
-            if (awe_ctrlGetModuleClass(&g_AWEInstance, AWE_SinkInt1_value_HANDLE, &classID) == OBJECT_FOUND)
+            if (awe_ctrlGetModuleClass(&AweInstance, AWE_SinkInt1_value_HANDLE, &classID) == OBJECT_FOUND)
             {
                 // Check that module assigned this object ID is of module class SinkInt
                 if (classID == AWE_SinkInt1_classID)
                 {
                     // SinkInt module (gets nValue from the running layout)
-                    awe_ctrlGetValue(&g_AWEInstance, AWE_SinkInt1_value_HANDLE, &nValue, 0, 1);
+                    awe_ctrlGetValue(&AweInstance, AWE_SinkInt1_value_HANDLE, &nValue, 0, 1);
 
                 }
             }
 
             // Does the current AWE model have a DCSourceInt module with this control object ID?
-            if (awe_ctrlGetModuleClass(&g_AWEInstance, AWE_DC1_value_HANDLE, &classID) == OBJECT_FOUND)
+            if (awe_ctrlGetModuleClass(&AweInstance, AWE_DC1_value_HANDLE, &classID) == OBJECT_FOUND)
             {
                 // Check that module assigned this object ID is of module class DCSourceInt
                 if (classID == AWE_DC1_classID)
                 {
                     // DCSourceInt module (returns nValue to the running layout)
-                    awe_ctrlSetValue(&g_AWEInstance, AWE_DC1_value_HANDLE, &nValue, 0, 1);
+                    awe_ctrlSetValue(&AweInstance, AWE_DC1_value_HANDLE, &nValue, 0, 1);
                 }
             }
         }
