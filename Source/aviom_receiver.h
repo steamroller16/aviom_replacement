283f07e73b74e20c8245c94592b869b3744fe23a
SSoundar <SSoundar@slb.com>
2020-12-21 19:11:06 -0600

    Moved relevant code from main to new aviom_receiver module.

---
+++ Source/aviom_receiver.h
@@ -0,0 +1,24 @@
+/*-----------------------------------------------------------------------------
+File Name   :
+Author      : Sam Soundar
+Date        : yyyy-mm-dd
+Description :
+------------------------------------------------------------------------------*/
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+/* Includes ------------------------------------------------------------------*/
+
+
+/* Public Macros/Constants/Structures ----------------------------------------*/
+
+
+/* Public Variables ----------------------------------------------------------*/
+
+
+/* Public Function Declarations ----------------------------------------------*/
+void AviomReceiver_Init(void);
+void AviomReceiver_MainLoopProcess(void);
+
+
+#endif
